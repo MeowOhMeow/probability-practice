55f503d02e351468657073a408dc47c62f3f3197
MeowOhMeow <abc22681157@gmail.com>
2023-05-20 11:54:17 +0800

    hi

---
+++ 實作.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from rake_nltk import Rake
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+from ast import literal_eval
+
+
